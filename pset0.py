99b2c91f40dcf625ff3bfd73240200b7bad01e2a
Alley Robinson <alrobinson@knights.ucf.edu>
2021-03-03 11:15:24 -0700

    Create pset0.py

---
+++ pset0.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  3 11:07:47 2021
+
+@author: Alley
+"""
+import math
+
+# Variable input comes in as a string.
+x = input("Enter your number x: ")
+y = input("Enter your number y: ")
+
+# Casting to Integer
+x = int(x)
+y = int(y)
+
+
+print(x ** y)
+print(math.log2(x))
